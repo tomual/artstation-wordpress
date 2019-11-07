2cb29ee43cf350fccbd7f5ff04cc9d7b4b07de2f
tomual <tomual@users.noreply.github.com>
2019-11-07 21:38:03 +0800

    Header, footer static

--- wp-content/themes/artstation-wordpress/footer.php
+++ wp-content/themes/artstation-wordpress/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,26 +12,26 @@
 
 ?>
 
-	</div><!-- #content -->
+<footer class="">
+	<div class="container d-flex justify-content-between">
+		<div>
+			<a href=""><img class="mr-3" height="20" width="20" src="instagram.svg"></a>
+			<a href=""><img class="mr-3" height="20" width="20" src="artstation.svg"></a>
+			<a href=""><img class="mr-3" height="20" width="20" src="itch-dot-io.svg"></a>
+		</div>
+		<div>
+			<a class="text-muted text-decoration-none" href=""><img height="20" width="20" src="minutemailer.svg"> Contact Me</a>
+		</div>
+		<div class="small text-muted">
+			© 2019 soulant. All rights reserved.
+		</div>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'artstation-wordpress' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'artstation-wordpress' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'artstation-wordpress' ), 'artstation-wordpress', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	</div>
+</footer>
 
-<?php wp_footer(); ?>
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" us"></script>
 </body>
+
 </html>
--- wp-content/themes/artstation-wordpress/header.php
+++ wp-content/themes/artstation-wordpress/header.php
@@ -24,38 +24,31 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'artstation-wordpress' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$artstation_wordpress_description = get_bloginfo( 'description', 'display' );
-			if ( $artstation_wordpress_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $artstation_wordpress_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+<header>
+	<nav class="navbar navbar-expand-lg navbar-dark py-4 px-5">
+		<div class="container">
+			<a class="navbar-brand" href="index.html">soulant</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'artstation-wordpress' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="list.html">Concept Art</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="list.html">3D Modeling</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="list.html">Projects</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="list.html">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
