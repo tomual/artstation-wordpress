52dc46e29f409b1e070d3737e5308abf8fff408d
tomual <tomual@users.noreply.github.com>
2019-11-22 15:38:02 +0800

    404 page

--- 404.php
+++ 404.php
@@ -10,51 +10,26 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="container">
+		<main id="main" class="row">
 
-			<section class="error-404 not-found">
+			<section class="error-404 not-found col">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'artstation-wordpress' ); ?></h1>
-				</header><!-- .page-header -->
+				</header>
 
-				<div class="page-content">
+				<div class="page-content text-center mb-5 pb-5">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'artstation-wordpress' ); ?></p>
 
 					<?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'artstation-wordpress' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
+				</div>
+			</section>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$artstation_wordpress_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'artstation-wordpress' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$artstation_wordpress_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
 get_footer();
